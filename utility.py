5be2feae69f0d2308f047dc6291f853688d726a4
shruti202X <87233649+shruti202X@users.noreply.github.com>
2025-11-30 20:01:00 +0530

    Enhance merge_tasks to support CSV and Excel files
    Updated merge_tasks function to handle both CSV and Excel files, including reading, merging, and saving. Added sorting by 'Date Time Standard' and removed the temporary column after processing.

--- utility.py
+++ utility.py
@@ -1,22 +1,37 @@
 import pandas as pd
 import os
+import csv
 
 def merge_tasks(task_file_name, other_task_files):
     
     # Read the main task file
-    task_df = pd.read_csv(task_file_name)
-    
-    # Merge with other task files
-    for other_task_file in other_task_files:
-        other_task_df = pd.read_csv(other_task_file)
-        task_df = pd.concat([task_df, other_task_df], ignore_index=True)
+    if task_file_name.endswith(".xlsx"):
+        task_df = pd.read_excel(task_file_name)
+        for other_task_file in other_task_files:
+            other_task_df = pd.read_excel(other_task_file)
+            task_df = pd.concat([task_df, other_task_df], ignore_index=True)
+    else:
+        task_df = pd.read_csv(task_file_name)
+        for other_task_file in other_task_files:
+            other_task_df = pd.read_csv(other_task_file)
+            task_df = pd.concat([task_df, other_task_df], ignore_index=True)
         
     # Sort by 'Date Time' column
-    task_df['Date Time'] = pd.to_datetime(task_df['Date Time'])
-    task_df = task_df.sort_values(by='Date Time').reset_index(drop=True)
+    task_df['Date Time Standard'] = pd.to_datetime(task_df['Date Time'], format='%d %B, %Y (%I:%M %p)')
+    # Date Time Standard is like YYYY-MM-DD HH:MM:SS
+
+    # Sort based on Date Time Data Type Column
+    task_df = task_df.sort_values(by='Date Time Standard').reset_index(drop=True)
+
+    # Remove Date Time Standard Column
+    task_df = task_df.drop(columns=['Date Time Standard'])
     
     # Save back to task_file_name
-    task_df.to_csv(task_file_name, index=False)
+    if task_file_name.endswith(".xlsx"):
+        with pd.ExcelWriter(task_file_name) as writer:
+            task_df.to_excel(writer, sheet_name="Log", index=False)
+    else:
+        task_df.to_csv(task_file_name, index=False)
     
     # Delete other_task_files
     for other_task_file in other_task_files:
